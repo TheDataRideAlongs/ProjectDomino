09516d970f31a86640121a416dbc7e6104b9bf52
webcoderz <19884161+webcoderz@users.noreply.github.com>
2020-08-06 18:44:41 -0500

    adding twint source into modules

---
+++ modules/Twint/twint/get.py
@@ -0,0 +1,235 @@
+from async_timeout import timeout
+from datetime import datetime
+from bs4 import BeautifulSoup
+import sys
+import socket
+import aiohttp
+from fake_useragent import UserAgent
+import asyncio
+import concurrent.futures
+import random
+from json import loads
+from aiohttp_socks import ProxyConnector, ProxyType
+
+from . import url
+from .output import Tweets, Users
+from .user import inf
+
+import logging as logme
+
+httpproxy = None
+
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
+def get_connector(config):
+    logme.debug(__name__+':get_connector')
+    _connector = None
+    if config.Proxy_host:
+        if config.Proxy_host.lower() == "tor":
+            _connector = ProxyConnector(
+                socks_ver=ProxyType.SOCKS5,
+                host='127.0.0.1',
+                port=9050,
+                rdns=True)
+        elif config.Proxy_port and config.Proxy_type:
+            if config.Proxy_type.lower() == "socks5":
+                _type = ProxyType.SOCKS5
+            elif config.Proxy_type.lower() == "socks4":
+                _type = ProxyType.SOCKS4
+            elif config.Proxy_type.lower() == "http":
+                global httpproxy
+                httpproxy = "http://" + config.Proxy_host + ":" + str(config.Proxy_port)
+                return _connector
+            else:
+                logme.critical("get_connector:proxy-type-error")
+                print("Error: Proxy types allowed are: http, socks5 and socks4. No https.")
+                sys.exit(1)
+            _connector = ProxyConnector(
+                socks_ver=_type,
+                host=config.Proxy_host,
+                port=config.Proxy_port,
+                rdns=True)
+        else:
+            logme.critical(__name__+':get_connector:proxy-port-type-error')
+            print("Error: Please specify --proxy-host, --proxy-port, and --proxy-type")
+            sys.exit(1)
+    else:
+        if config.Proxy_port or config.Proxy_type:
+            logme.critical(__name__+':get_connector:proxy-host-arg-error')
+            print("Error: Please specify --proxy-host, --proxy-port, and --proxy-type")
+            sys.exit(1)
+
+    return _connector
+
+
+async def RequestUrl(config, init, headers = []):
+    logme.debug(__name__+':RequestUrl')
+    _connector = get_connector(config)
+    _serialQuery = ""
+    params = []
+    _url = ""
+
+    if config.Profile:
+        if config.Profile_full:
+            logme.debug(__name__+':RequestUrl:Profile_full')
+            _url = await url.MobileProfile(config.Username, init)
+        else:
+            logme.debug(__name__+':RequestUrl:notProfile_full')
+            _url = await url.Profile(config.Username, init)
+        _serialQuery = _url
+    elif config.TwitterSearch:
+        logme.debug(__name__+':RequestUrl:TwitterSearch')
+        _url, params, _serialQuery = await url.Search(config, init)
+    else:
+        if config.Following:
+            logme.debug(__name__+':RequestUrl:Following')
+            _url = await url.Following(config.Username, init)
+        elif config.Followers:
+            logme.debug(__name__+':RequestUrl:Followers')
+            _url = await url.Followers(config.Username, init)
+        else:
+            logme.debug(__name__+':RequestUrl:Favorites')
+            _url = await url.Favorites(config.Username, init)
+        _serialQuery = _url
+
+    response = await Request(_url, params=params, connector=_connector, headers=headers)
+
+    if config.Debug:
+        print(_serialQuery, file=open("twint-request_urls.log", "a", encoding="utf-8"))
+
+    return response
+
+def ForceNewTorIdentity(config):
+    logme.debug(__name__+':ForceNewTorIdentity')
+    try:
+        tor_c = socket.create_connection(('127.0.0.1', config.Tor_control_port))
+        tor_c.send('AUTHENTICATE "{}"\r\nSIGNAL NEWNYM\r\n'.format(config.Tor_control_password).encode())
+        response = tor_c.recv(1024)
+        if response != b'250 OK\r\n250 OK\r\n':
+            sys.stderr.write('Unexpected response from Tor control port: {}\n'.format(response))
+            logme.critical(__name__+':ForceNewTorIdentity:unexpectedResponse')
+    except Exception as e:
+        logme.debug(__name__+':ForceNewTorIdentity:errorConnectingTor')
+        sys.stderr.write('Error connecting to Tor control port: {}\n'.format(repr(e)))
+        sys.stderr.write('If you want to rotate Tor ports automatically - enable Tor control port\n')
+
+async def Request(url, connector=None, params=[], headers=[]):
+    logme.debug(__name__+':Request:Connector')
+    async with aiohttp.ClientSession(connector=connector, headers=headers) as session:
+        return await Response(session, url, params)
+
+async def Response(session, url, params=[]):
+    logme.debug(__name__+':Response')
+    with timeout(120):
+        async with session.get(url, ssl=True, params=params, proxy=httpproxy) as response:
+            return await response.text()
+
+async def RandomUserAgent(wa=None):
+    logme.debug(__name__+':RandomUserAgent')
+    try:
+        if wa:
+            return "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36"
+        return UserAgent(verify_ssl=False, use_cache_server=False).random
+    except:
+        return random.choice(user_agent_list)
+
+async def Username(_id):
+    logme.debug(__name__+':Username')
+    url = f"https://twitter.com/intent/user?user_id={_id}&lang=en"
+    r = await Request(url, headers={"X-Requested-With": "XMLHttpRequest"})
+    soup = BeautifulSoup(r, "html.parser")
+
+    return soup.find("a", "fn url alternate-context")["href"].replace("/", "")
+
+async def Tweet(url, config, conn):
+    logme.debug(__name__+':Tweet')
+    try:
+        response = await Request(url)
+        soup = BeautifulSoup(response, "html.parser")
+        tweets = soup.find_all("div", "tweet")
+        await Tweets(tweets, config, conn, url)
+    except Exception as e:
+        logme.critical(__name__+':Tweet:' + str(e))
+
+async def User(url, config, conn, user_id = False):
+    logme.debug(__name__+':User')
+    _connector = get_connector(config)
+    try:
+        response = await Request(url, connector=_connector, headers={"X-Requested-With": "XMLHttpRequest"})
+        soup = BeautifulSoup(response, "html.parser")
+        if user_id:
+            return int(inf(soup, "id"))
+        await Users(soup, config, conn)
+    except Exception as e:
+        logme.critical(__name__+':User:' + str(e))
+
+def Limit(Limit, count):
+    logme.debug(__name__+':Limit')
+    if Limit is not None and count >= int(Limit):
+        return True
+
+async def Multi(feed, config, conn):
+    logme.debug(__name__+':Multi')
+    count = 0
+    try:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            loop = asyncio.get_event_loop()
+            futures = []
+            for tweet in feed:
+                count += 1
+                if config.Favorites or config.Profile_full:
+                    logme.debug(__name__+':Multi:Favorites-profileFull')
+                    link = tweet.find("a")["href"]
+                    url = f"https://twitter.com{link}&lang=en"
+                elif config.User_full:
+                    logme.debug(__name__+':Multi:userFull')
+                    username = tweet.find("a")["name"]
+                    url = f"http://twitter.com/{username}?lang=en"
+                else:
+                    logme.debug(__name__+':Multi:else-url')
+                    link = tweet.find("a", "tweet-timestamp js-permalink js-nav js-tooltip")["href"]
+                    url = f"https://twitter.com{link}?lang=en"
+
+                if config.User_full:
+                    logme.debug(__name__+':Multi:user-full-Run')
+                    futures.append(loop.run_in_executor(executor, await User(url,
+                        config, conn)))
+                else:
+                    logme.debug(__name__+':Multi:notUser-full-Run')
+                    futures.append(loop.run_in_executor(executor, await Tweet(url,
+                        config, conn)))
+            logme.debug(__name__+':Multi:asyncioGather')
+            await asyncio.gather(*futures)
+    except Exception as e:
+        # TODO: fix error not error
+        # print(str(e) + " [x] get.Multi")
+        # will return "'NoneType' object is not callable"
+        # but still works
+        # logme.critical(__name__+':Multi:' + str(e))
+        pass
+
+    return count
---
+++ modules/Twint/twint/tweet.py
@@ -0,0 +1,133 @@
+from time import strftime, localtime
+from datetime import datetime
+import json
+
+import logging as logme
+from googletransx import Translator
+
+# ref. 
+# - https://github.com/x0rzkov/py-googletrans#basic-usage
+translator = Translator()
+
+class tweet:
+    """Define Tweet class
+    """
+    type = "tweet"
+
+    def __init__(self):
+        pass
+
+def getMentions(tw):
+    """Extract ment from tweet
+    """
+    logme.debug(__name__+':getMentions')
+    try:
+        mentions = tw["data-mentions"].split(" ")
+    except:
+        mentions = []
+
+    return mentions
+
+def getQuoteURL(tw):
+    """Extract quote from tweet
+    """
+    logme.debug(__name__+':getQuoteURL')
+    base_twitter = "https://twitter.com"
+    quote_url = ""
+    try:
+        quote = tw.find("div","QuoteTweet-innerContainer")
+        quote_url = base_twitter + quote.get("href")
+    except:
+        quote_url = ""
+
+    return quote_url
+
+def getText(tw):
+    """Replace some text
+    """
+    logme.debug(__name__+':getText')
+    text = tw.find("p", "tweet-text").text
+    text = text.replace("http", " http")
+    text = text.replace("pic.twitter", " pic.twitter")
+
+    return text
+
+def getStat(tw, _type):
+    """Get stats about Tweet
+    """
+    logme.debug(__name__+':getStat')
+    st = f"ProfileTweet-action--{_type} u-hiddenVisually"
+    return tw.find("span", st).find("span")["data-tweet-stat-count"]
+
+def getRetweet(tw, _config):
+    """Get Retweet
+    """
+    logme.debug(__name__+':getRetweet')
+    if _config.Profile:
+        if int(tw["data-user-id"]) != _config.User_id:
+            return _config.User_id, _config.Username
+    else:
+        _rt_object = tw.find('span', 'js-retweet-text')
+        if _rt_object:
+            _rt_id = _rt_object.find('a')['data-user-id']
+            _rt_username = _rt_object.find('a')['href'][1:]
+            return  _rt_id, _rt_username
+    return '', ''
+
+def Tweet(tw, config):
+    """Create Tweet object
+    """
+    logme.debug(__name__+':Tweet')
+    t = tweet()
+    t.id = int(tw["data-item-id"])
+    t.id_str = tw["data-item-id"]
+    t.conversation_id = tw["data-conversation-id"]
+    t.datetime = int(tw.find("span", "_timestamp")["data-time-ms"])
+    t.datestamp = strftime("%Y-%m-%d", localtime(t.datetime/1000.0))
+    t.timestamp = strftime("%H:%M:%S", localtime(t.datetime/1000.0))
+    t.user_id = int(tw["data-user-id"])
+    t.user_id_str = tw["data-user-id"]
+    t.username = tw["data-screen-name"]
+    t.name = tw["data-name"]
+    t.place = tw.find("a","js-geo-pivot-link").text.strip() if tw.find("a","js-geo-pivot-link") else ""
+    t.timezone = strftime("%z", localtime())
+    for img in tw.findAll("img", "Emoji Emoji--forText"):
+        img.replaceWith(img["alt"])
+    t.mentions = getMentions(tw)
+    t.urls = [link.attrs["data-expanded-url"] for link in tw.find_all('a',{'class':'twitter-timeline-link'}) if link.has_attr("data-expanded-url")]
+    t.photos = [photo_node.attrs['data-image-url'] for photo_node in tw.find_all("div", "AdaptiveMedia-photoContainer")]
+    t.video = 1 if tw.find_all("div", "AdaptiveMedia-video") != [] else 0
+    t.tweet = getText(tw)
+    t.lang = tw.find('p', 'tweet-text')['lang']
+    t.hashtags = [hashtag.text for hashtag in tw.find_all("a","twitter-hashtag")]
+    t.cashtags = [cashtag.text for cashtag in tw.find_all("a", "twitter-cashtag")]
+    t.replies_count = getStat(tw, "reply")
+    t.retweets_count = getStat(tw, "retweet")
+    t.likes_count = getStat(tw, "favorite")
+    t.link = f"https://twitter.com/{t.username}/status/{t.id}"
+    t.user_rt_id, t.user_rt = getRetweet(tw, config)
+    t.retweet = True if t.user_rt else False
+    t.retweet_id = ''
+    t.retweet_date = ''
+    if not config.Profile:
+        t.retweet_id = tw['data-retweet-id'] if t.user_rt else ''
+        t.retweet_date = datetime.fromtimestamp(((int(t.retweet_id) >> 22) + 1288834974657)/1000.0).strftime("%Y-%m-%d %H:%M:%S") if t.user_rt else ''
+    t.quote_url = getQuoteURL(tw)
+    t.near = config.Near if config.Near else ""
+    t.geo = config.Geo if config.Geo else ""
+    t.source = config.Source if config.Source else ""
+    t.reply_to = [{'user_id': t['id_str'], 'username': t['screen_name']} for t in json.loads(tw["data-reply-to-users-json"])]
+    t.translate = ''
+    t.trans_src = ''
+    t.trans_dest = ''
+    if config.Translate == True:
+        try:
+            ts = translator.translate(text=t.tweet, dest=config.TranslateDest)
+            t.translate = ts.text
+            t.trans_src = ts.src
+            t.trans_dest = ts.dest
+        # ref. https://github.com/SuniTheFish/ChainTranslator/blob/master/ChainTranslator/__main__.py#L31
+        except ValueError as e:
+            raise Exception("Invalid destination language: {} / Tweet: {}".format(config.TranslateDest, t.tweet))
+            logme.debug(__name__+':Tweet:translator.translate:'+str(e))
+    return t
