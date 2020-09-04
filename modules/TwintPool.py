41bf592de0ac27913b1e31bb6d3904f7a830a81b
webcoderz <19884161+webcoderz@users.noreply.github.com>
2020-09-03 23:33:52 -0500

    Update TwintPool.py

--- modules/TwintPool.py
+++ modules/TwintPool.py
@@ -18,9 +18,9 @@ class TwintPool:
         self.config.Verified = None
         self.config.Username = None
         #self.config.User_full = True
-        #self.config.Proxy_host = "tor"
-        #self.config.Proxy_port = "9050"
-        #self.config.Proxy_type = "socks5"
+        self.config.Proxy_host = None #"tor"
+        self.config.Proxy_port = None #"9050"
+        self.config.Proxy_type = None #"socks5"
 
     def twint_loop(self, since, until, stride_sec=600, limit=None):
         def get_unix_time(time_str):
