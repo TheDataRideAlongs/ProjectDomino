74626330f0f56d25d3a32e03832d4df7aa48d5e6
webcoderz <19884161+webcoderz@users.noreply.github.com>
2020-09-04 23:03:34 -0500

    enrich usr info fx change can enrich any df with df["user_screen_name"] ,changed save to writer for fh.search_time_range, , added acct enrichment to save tweet_df_to_neo

--- modules/TwintPool.py
+++ modules/TwintPool.py
@@ -1,14 +1,16 @@
 import pyarrow as pa
-import twint
+from Twint import twint
 from urlextract import URLExtract
 from datetime import datetime, timedelta
 import pandas as pd
 import json
 import logging
+
 logger = logging.getLogger()
 
+
 class TwintPool:
-    
+
     def __init__(self, fh_job=None, job_name='noname'):
         self.fh = fh_job
         self.config = twint.Config()
@@ -18,21 +20,22 @@ class TwintPool:
         self.config.Verified = None
         self.config.Username = None
         self.config.User_full = None
-        self.config.Proxy_host = None #"tor"
-        self.config.Proxy_port = None #"9050"
-        self.config.Proxy_type = None #"socks5"
+        self.config.Proxy_host = None  # "tor"
+        self.config.Proxy_port = None  # "9050"
+        self.config.Proxy_type = None  # "socks5"
 
     def twint_loop(self, since, until, stride_sec=600, limit=None):
         def get_unix_time(time_str):
             return datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+
         since = get_unix_time(since)
         until = get_unix_time(until)
         t = since
         tweets_returned = 0
-        
+
         while t < until and (not tweets_returned or tweets_returned < limit):
             t0 = t
-            t1 = t+timedelta(seconds=stride_sec)
+            t1 = t + timedelta(seconds=stride_sec)
             self.config.Since = str(t0)
             self.config.Until = str(t1)
             logger.debug('Search step: %s-%s', t0, t1)
@@ -45,45 +48,42 @@ class TwintPool:
                 logger.debug('not hits on %s - %s, continuing', t0, t1)
             t = t1
         logger.debug('twint_loop done, hits: %s', tweets_returned)
-                
 
-        
-    def _get_term(self, Search="IngSoc", Since="1984-04-20 13:00:00", Until="1984-04-20 13:30:00", stride_sec=600, **kwargs):
+    def _get_term(self, Search="IngSoc", Since="1984-04-20 13:00:00", Until="1984-04-20 13:30:00", stride_sec=600,
+                  **kwargs):
         self.config.Search = Search
         self.config.Retweets = True
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             setattr(self.config, k, v)
-        #self.config.Search = term
+        # self.config.Search = term
         logger.debug('Search seq: %s-%s of %s', Search, Since, Until)
-        for df,t0,t1 in self.twint_loop(Since, Until, stride_sec, self.config.Limit):
+        for df, t0, t1 in self.twint_loop(Since, Until, stride_sec, self.config.Limit):
             yield (df, t0, t1)
-    
+
     def _get_timeline(self, username, limit):
         self.config.Retweets = True
-        self.config.Search = "from:"+username
+        self.config.Search = "from:" + username
         self.config.Limit = limit
         twint.run.Search(self.config)
         tweets_df = twint.storage.panda.Tweets_df
         return tweets_df
 
-    def twint_get(self,Search,Since,Until,job_name,Limit):
+    def twint_get(self, Search, Since, Until, job_name, Limit, **kwargs):
         from .FirehoseJob import FirehoseJob
         neo4j_creds = None
         with open('../neo4jcreds.json') as json_file:
             neo4j_creds = json.load(json_file)
-        fh = FirehoseJob( neo4j_creds=neo4j_creds, PARQUET_SAMPLE_RATE_TIME_S=30, save_to_neo=False, writers={})
+        fh = FirehoseJob(neo4j_creds=neo4j_creds, PARQUET_SAMPLE_RATE_TIME_S=30, save_to_neo=False, writers={})
         dfs = []
         for df in fh.search_time_range(
-            Search=Search,
-            Since=Since,
-            Until=Until,
-            job_name=job_name,
-            Limit=Limit):
+                Search=Search,
+                Since=Since,
+                Until=Until,
+                job_name=job_name,
+                Limit=Limit):
             dfs.append(df)
         df = pd.concat(dfs)
         return df
-
-
 
     def _get_user_info(self, username):
         self.config.User_full = True
@@ -92,10 +92,10 @@ class TwintPool:
         twint.run.Lookup(self.config)
         return twint.storage.panda.User_df
 
-    def check_hydrate(self,df):
-        
+    def check_hydrate(self, df):
+
         df = df.assign(id=df['id'].astype('int64'))
-        
+
         from .Neo4jDataAccess import Neo4jDataAccess
         neo4j_creds = None
         with open('../neo4jcreds.json') as json_file:
@@ -103,27 +103,24 @@ class TwintPool:
 
         # dft : df[[id:int64, hydrated: NaN | 'FULL' | 'PARTIAL'??]]
         dft = Neo4jDataAccess(neo4j_creds=neo4j_creds).get_tweet_hydrated_status_by_id(df)
-        needs_hydrate_ids = dft[ dft['hydrated'] != 'FULL' ][['id']]
+        needs_hydrate_ids = dft[dft['hydrated'] != 'FULL'][['id']]
         needs_hydrate_df = df.merge(needs_hydrate_ids, how='inner', on='id')
-        
+
         return needs_hydrate_df
-        
-    
-    
+
     def twint_df_to_neo4j_df(self, df):
-        #df=self.__check_hydrate(df)
+        # df=self.__check_hydrate(df)
         neo4j_df = df.rename(columns={
             'id': 'status_id',
             'tweet': 'full_text',
-            'created_at': 'created_at', # needs to be datetime
+            'created_at': 'created_at',  # needs to be datetime
             'nlikes': 'favorite_count',
             'nretweets': 'retweet_count',
-            #'user_id_str': 'user_id',
+            # 'user_id_str': 'user_id',
             'username': 'user_name',
             'name': 'user_screen_name'
-            })
-        
-        
+        })
+
         def row_to_tweet_type(row):
             if row['quote_url'] is None or row['quote_url'] == '':
                 return "QUOTE_RETWEET"
@@ -134,45 +131,43 @@ class TwintPool:
             elif row['id'] != row['conversation_id']:
                 return "REPLY"
             else:
-                raise('wat')
-        
-        #def row_to_quoted_status_id(row):
-            #if row['quote_url'] and len(row['quote_url']) > 0:
-                #return row['quote_url'].split('/')[-1]
-            #else:
-                #return None
-            
+                raise ('wat')
+
+        # def row_to_quoted_status_id(row):
+        # if row['quote_url'] and len(row['quote_url']) > 0:
+        # return row['quote_url'].split('/')[-1]
+        # else:
+        # return None
+
         def row_tweet_to_urls(row):
             extractor = URLExtract()
             return list(extractor.gen_urls(row['tweet']))
-            
+
         neo4j_df['user_location'] = None
         neo4j_df['tweet_type_twint'] = df.apply(row_to_tweet_type, axis=1)
         neo4j_df['hashtags'] = df['hashtags'].apply(lambda x: [{'text': ht} for ht in x])
         neo4j_df['user_followers_count'] = None
         neo4j_df['user_friends_count'] = None
-        #neo4j_df['user_created_at'] = None
+        # neo4j_df['user_created_at'] = None
         neo4j_df['user_profile_image_url'] = None
         neo4j_df['reply_tweet_id'] = None
         neo4j_df['user_mentions'] = df['tweet'].str.findall('@[\w]+')
-        #neo4j_df['retweet_id'] is suspiciously empty (always)
+        # neo4j_df['retweet_id'] is suspiciously empty (always)
         neo4j_df['retweeted_status'] = None
-        neo4j_df['conversation_id'] = df['conversation_id'] #FIXME no-op?
+        neo4j_df['conversation_id'] = df['conversation_id']  # FIXME no-op?
         neo4j_df['created_at'] = (neo4j_df['created_at'] / 1000).apply(lambda n: datetime.fromtimestamp(n))
-        
-        #neo4j_df['quoted_status_id'] = df.apply(row_to_quoted_status_id, axis=1)
-        #neo4j_df['is_quote_status'] = neo4j_df['quoted_status_id'] != None
+
+        # neo4j_df['quoted_status_id'] = df.apply(row_to_quoted_status_id, axis=1)
+        # neo4j_df['is_quote_status'] = neo4j_df['quoted_status_id'] != None
         neo4j_df['in_reply_to_status_id'] = False
         neo4j_df['urls'] = df.apply(row_tweet_to_urls, axis=1)
-        neo4j_df['user_id']= df['user_id']
+        neo4j_df['user_id'] = df['user_id']
         return neo4j_df
-        
 
     def to_arrow(self, tweets_df):
         pass
-        
-     
-        
 
-     
-        
+
+
+
+
