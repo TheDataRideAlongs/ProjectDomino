ebe46c40831ed46d29584d0382cbc69942bb6d24
webcoderz <19884161+webcoderz@users.noreply.github.com>
2020-09-09 22:44:27 -0500

    reset_tables.sh

---
+++ infra/pipelines/docker/jobs/reset_tables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# to run iptables commands you need to be root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    return 1
+fi
+
+echo 'current rules are: '
+
+iptables -S
+
+echo 'flushing... '
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+echo 'current rules are: '
+
+iptables -S
